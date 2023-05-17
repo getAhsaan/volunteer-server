701d7c37223c4af971d4be65a879c9a68217bddd
getahsaan <getahsaan@gamil.com>
2023-05-17 16:53:04 +0600

    delete operation added

--- index.js
+++ index.js
@@ -52,6 +52,13 @@ async function run() {
       res.send(await volunteerCollection.insertOne(req.body));
     });
 
+    app.delete("/posts/:id", async (req, res) => {
+      const result = await volunteerCollection.deleteOne({
+        _id: new ObjectId(req.params.id),
+      });
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
